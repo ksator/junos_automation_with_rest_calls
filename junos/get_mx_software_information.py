0a95c034ad4bab2dd175fbf93b40307c5409feb2
Khelil Sator <khelil.sator.git@gmail.com>
2018-01-15 11:54:54 +0100

    Create get_mx_software_information.py

---
+++ junos/get_mx_software_information.py
@@ -0,0 +1,19 @@
+"""
+DESCRIPTION: retrieve in a json representation the software information from an MX router with a REST API call. 
+the output is parsed and some details are printed
+the rpc get-software-information is the equivalent of 'show version'
+USAGE: python junos/get_mx_software_information.py
+"""
+
+import requests
+from pprint import pprint
+from requests.auth import HTTPBasicAuth
+
+my_headers = { 'Accept': 'application/json' }
+
+r = requests.get('http://172.30.177.170:3000/rpc/get-software-information', auth=HTTPBasicAuth('pytraining', 'Poclab123'), headers=my_headers)
+
+#pprint(r.json())
+print "Software version: " + r.json()['software-information'][0]['junos-version'][0]['data']
+print "Host-name: " + r.json()['software-information'][0]['host-name'][0]['data']
+print "Product name: " + r.json()['software-information'][0]['product-name'][0]['data']
