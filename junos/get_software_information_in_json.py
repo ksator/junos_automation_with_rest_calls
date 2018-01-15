d3f9f6d9dd89abcaa4a5669e14445b2d7a1e1e5c
Khelil Sator <khelil.sator.git@gmail.com>
2018-01-15 13:32:00 +0100

    Update get_software_information_in_json.py

--- junos/get_software_information_in_json.py
+++ junos/get_software_information_in_json.py
@@ -13,7 +13,9 @@ my_headers = { 'Accept': 'application/json' }
 
 r = requests.get('http://172.30.177.170:3000/rpc/get-software-information', auth=HTTPBasicAuth('pytraining', 'Poclab123'), headers=my_headers)
 
+#type(r.json())
 #pprint(r.json())
+
 print "Software version: " + r.json()['software-information'][0]['junos-version'][0]['data']
 print "Host-name: " + r.json()['software-information'][0]['host-name'][0]['data']
 print "Product name: " + r.json()['software-information'][0]['product-name'][0]['data']
