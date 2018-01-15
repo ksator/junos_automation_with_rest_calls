dcc033dc8d87f58f5765b3221282bfd8ddb42739
Khelil Sator <khelil.sator.git@gmail.com>
2018-01-15 13:27:33 +0100

    Update get_configuration_with_filter.py

--- junos/get_configuration_with_filter.py
+++ junos/get_configuration_with_filter.py
@@ -8,17 +8,12 @@ from requests.auth import HTTPBasicAuth
 from pprint import pprint
 
 headers = { 'content-type' : 'application/xml' }
- 
-#indicate below the device credentials 
 authuser = 'lab'
 authpwd = 'm0naco'
 
 # the default port for junos rest api is 3000. I am using the port 8080 for this device 
-
 # get the junos configuration with a filter
 
 payload = ('<get-config><source><running/></source><filter type="subtree"><configuration><interfaces><interface><name>ge-0/0/0</name></interface></interfaces></configuration></filter></get-config>')
-
 q = requests.post('http://172.30.52.152:8080/rpc', headers=headers, auth=HTTPBasicAuth(authuser, authpwd), data=payload)
-
 print q.content
